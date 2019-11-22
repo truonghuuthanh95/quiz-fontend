b226a7e9f63bac692d8f805b6380e99ee0969310
truonghuuthanh <truonghuuthanh95@gmail.com>
2019-11-22 10:37:35 +0700

    commit so cau

--- src/containers/Result/index.js
+++ src/containers/Result/index.js
@@ -25,7 +25,7 @@ class Result extends Component {
         <div style={{ color: "white" }} className="result text-center">
           <h1 style={{ color: "green" }}>NÔP BÀI THÀNH CÔNG</h1>
           <h2>
-            Đểm <b>{this.state.result.NumCorrect}/60</b>
+            Đểm <b>{this.state.result.NumCorrect}/30</b>
           </h2>
           <h2>Thời gian {this.state.result.TimeTaken.substring(3, 8)}</h2>
           <div className="pyro">
